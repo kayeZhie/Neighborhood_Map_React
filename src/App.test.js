c13e88253a765d0163197eaf3eb6ed5ccddc65ff
Karren Zoleta <zoletakarren@gmail.com>
2018-10-12 16:21:43 -0700

    installed all dependencies

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+
+			)
+	}
+}
+
+export default App;
