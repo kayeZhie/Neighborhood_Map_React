9a65e796a944c9b74b568e080b9f89bbcc876837
Karren Zoleta <zoletakarren@gmail.com>
2018-10-27 14:02:21 -0700

    Adds Json hard coded file, edit style.css for styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import SquareAPI from './API/foursquareAPI'
+
  class App extends Component {
+ 	componentDidMount() {
+ 		SquareAPI.search({
+ 			near:"San Francisco,CA", 
+ 			query: "pizza",
+ 			limit: 6
+ 		}).then(results => console.log(results));
+
+ 	}
   render() {
     return (
       <div className="App">
