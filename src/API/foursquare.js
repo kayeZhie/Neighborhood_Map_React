a2a3d87187964153caa68b401e65828a271e22af
Karren Zoleta <zoletakarren@gmail.com>
2018-10-30 19:23:49 -0700

    set up foursqaure API for the app

---
+++ src/API/foursquare.js
@@ -0,0 +1,58 @@
+class helper {
+	static baseURL() {
+		return "GET https://api.foursquare.com/v2";
+	}
+
+	static auth(){
+		const keys = {
+			client_id:"52WWWVIBTJFWH34X3CEPOFOO5420AZWISHUPMSDQIITR5Y3B",
+			client_secret:"BHSSTVQJDWVVIIWMN3BMAOLZ0SSBMILOFPG1RCBAVVJUKFDK",
+			v:"20181023"
+		};
+		return Object.keys(keys).map(key => `${key}=${keys[key]}`).join("&");
+	} 
+
+	static urlBuilder(urlPrams) {
+		if(!urlPrams) {
+			return ""
+		}
+		return Object.keys(urlPrams)
+			.map(key => `${key}=${urlPrams[key]}`).join("&");		
+
+	}
+
+	static headers() {
+		return {
+			accept:"application/json"
+		};
+	}
+
+	static simpleFetch(endPoint, method, urlPrams) {
+		let requestData = {
+			method,
+			headers: helper.headers()
+		};
+
+		return fetch(
+			`${helper.baseURL()}${endPoint}?${helper.auth()}&${helper.urlBuilder(
+			urlPrams
+			)}`,
+			requestData
+		).then(res => res.text());
+	}
+}
+
+export default class SquareAPI {
+	static search(urlPrams) {
+		return helper.simpleFetch("/venues/search", "GET", urlPrams);
+	}
+
+	static getVenueDetails(VENUE_ID) {
+		return helper.simpleFetch(`/venues/${VENUE_ID}`, "GET");
+	}
+
+	static getVenuePhotos(VENUE_ID) {
+		return helper.simpleFetch(`/venues/${VENUE_ID}/photos`, "GET");
+
+	}
+}
--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 import locations from './data/List.json';
 import PresentMap from './components/showMap';
+import SquareAPI from './API/foursquare'
 
 class App extends Component {
+componentDidMount() {
+        fetch('https://api.foursquare.com/v2/venues/search?client_id=52WWWVIBTJFWH34X3CEPOFOO5420AZWISHUPMSDQIITR5Y3B&client_secret=BHSSTVQJDWVVIIWMN3BMAOLZ0SSBMILOFPG1RCBAVVJUKFDK&v=20181023&near=San Bruno,CA&query=Filipinorestaurant&limit=10')
+        .then(res => res.json())
+        .then(data => console.log(data.response));     
+  }
+
   state = {
     lat: 37.6305,
     lon: -122.4111,
     zoom: 13,
-    all: locations
+    all: locations,
   }
 
   render = () => {
